71222f1d251fb3f2ffdbab6ea8bbc57beec1f66b
Thomas Gideon <cmdln@thecommandline.net>
2009-01-25 20:11:21 -0500

    Initial skeleton.

---
+++ commit-context.py
@@ -0,0 +1,14 @@
+#
+#  commit-context.py
+#  Build up some descriptive context for automatic commit to git
+#
+#  Created by Thomas Gideon on 1/25/2009
+#  TODO add license block
+
+if __name__ == "__main__":
+    import sys
+
+    print "time zone"
+    print "weather for time zone"
+    print "system up time"
+    print "last 3 entries from a feed"
