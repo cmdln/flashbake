2d66d40c0c72a924cd271d234d421e4f9751ed1c
Thomas Gideon <cmdln@thecommandline.net>
2009-02-02 19:24:27 -0500

    Added notice of orphans.

---
+++ commit_context.py
@@ -0,0 +1,179 @@
+#
+#  commit-context.py
+#  Build up some descriptive context for automatic commit to git
+#
+#  Created by Thomas Gideon (cmdln@thecommandline.net) on 1/25/2009
+#  Provided as-is, with no warranties
+#  License: http://creativecommons.org/licenses/by-nc-sa/3.0/us/ 
+
+import sys
+import urllib, urllib2
+import xml.dom.minidom
+import os
+import string
+import random
+
+def get_uptime():
+    """ copied with blanket permission from
+        http://thesmithfam.org/blog/2005/11/19/python-uptime-script/ """
+
+    try:
+         f = open( "/proc/uptime" )
+         contents = f.read().split()
+         f.close()
+    except:
+        return "Cannot open uptime file: /proc/uptime"
+
+    total_seconds = float(contents[0])
+
+    # Helper vars:
+    MINUTE  = 60
+    HOUR    = MINUTE * 60
+    DAY     = HOUR * 24
+
+    # Get the days, hours, etc:
+    days    = int( total_seconds / DAY )
+    hours   = int( ( total_seconds % DAY ) / HOUR )
+    minutes = int( ( total_seconds % HOUR ) / MINUTE )
+    seconds = int( total_seconds % MINUTE )
+
+    # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
+    string = ""
+    if days> 0:
+        string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+    if len(string)> 0 or hours> 0:
+        string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
+    if len(string)> 0 or minutes> 0:
+        string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
+    string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
+
+    return string
+
+def get_weather(city):
+    """ This relies on Google's unpublished weather API which may change without notice. """
+
+    # unpublished API that iGoogle uses for its weather widget
+    baseurl = 'http://www.google.com/ig/api?'
+    # encode the sole paramtere
+    for_city = baseurl + urllib.urlencode({'weather': city})
+
+    # necessary machinery to fetch a web page
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+
+    # open the weather API page
+    weather_xml = opener.open(urllib2.Request(for_city)).read()
+
+    # the weather API returns some nice, parsable XML
+    weather_dom = xml.dom.minidom.parseString(weather_xml)
+
+    # just interested in the conditions at the moment
+    current = weather_dom.getElementsByTagName("current_conditions")
+
+    weather = {}
+    for child in current[0].childNodes:
+       if child.localName == 'icon':
+           continue
+       weather[child.localName] = child.getAttribute('data').strip()
+
+    return weather
+
+def get_text(nodelist):
+    text_value = ""
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            text_value = text_value + node.data
+    return text_value
+
+def get_rss(url, limit, creator):
+    """ Fetch up to the limit number of items from the specified feed with the specified
+        creator. """
+
+    # necessary machinery to fetch a web page
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+
+    # open the raw RSS XML
+    rss_xml = opener.open(urllib2.Request(url)).read()
+
+    # build a mini-dom so we can scrape out titles, descriptions
+    rss_dom = xml.dom.minidom.parseString(rss_xml)
+
+    titles = rss_dom.getElementsByTagName("title")
+
+    feed_title = get_text(titles[0].childNodes)
+
+    items = rss_dom.getElementsByTagName("item")
+
+    by_creator = []
+    for child in items:
+       item_creator = child.getElementsByTagName("dc:creator")[0]
+       item_creator = get_text(item_creator.childNodes).strip()
+       if item_creator != creator:
+           continue
+       title = get_text(child.getElementsByTagName("title")[0].childNodes)
+       link = get_text(child.getElementsByTagName("link")[0].childNodes)
+       by_creator.append({"title" : title, "link" : link})
+       if limit <= len(by_creator):
+           break
+
+    return (feed_title, by_creator)
+
+def build_message_file(feed_url, item_limit, by_line):
+    # check the environment for the zone value
+    zone = os.environ.get("TZ")
+
+    # some desktops don't set the env var but /etc/timezone should
+    # have the value regardless
+    if None == zone:
+        zone_file = open('/etc/timezone')
+        zone = zone_file.read()
+        zone = zone.replace("\n", "")
+
+    tokens = zone.split("/")
+    if len(tokens) != 2:
+        print 'Zone id, "', zone, '", doesn''t appear to contain a city.'
+        # return non-zero so calling shell script can catch
+        return 1
+
+    city = tokens[1]
+    # ISO id's have underscores, convert to spaces for the Google API
+    city = city.replace("_", " ")
+
+    # call the Google weather API with the city
+    weather = get_weather(city)
+
+    # with thanks to Dave Smith
+    uptime = get_uptime()
+    
+    # last n items for m creator
+    (title,last_items) = get_rss(feed_url, item_limit, by_line)
+
+    msg_filename = '/tmp/git_msg_%d' % random.randint(0,100)
+    message_file = open(msg_filename, 'w')
+    try:
+        message_file.write('Current time zone is %s\n' % zone)
+        # there is also an entry for the key, wind_condition, in the weather
+        # dictionary
+        message_file.write('Current weather is %(condition)s (%(temp_f)sF/%(temp_c)sC) %(humidity)s\n' % weather)
+        message_file.write('System has been up %s\n' % uptime)
+        message_file.write('Last %(item_count)d entries from %(feed_title)s:\n' % {'item_count' : len(last_items), 'feed_title' : title})
+        for item in last_items:
+          # edit the '%s' if you want to add a label, like 'Title %s' to the output
+          message_file.write('%s\n' % item['title'])
+          message_file.write('%s\n' % item['link'])
+    finally:
+        message_file.close()
+    return msg_filename
+
+# call go() when this module is executed as the main script
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print '%s <feed url> <item limit> <author name>' % sys.argv[0]
+        sys.exit(1)
+    msg_filename = build_message_file(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    message_file = open(msg_filename, 'r')
+
+    try:
+        for line in message_file:
+            print line.strip()
+    finally:
+        message_file.close()
---
+++ commit_wrapper.py
@@ -0,0 +1,137 @@
+#
+#  commit-wrapper.py
+#  Parses a project's control file and wraps git operations, calling the context
+#  script to build automatic commit messages as needed.
+#
+#  Created by Thomas Gideon (cmdln@thecommandline.net) on 1/25/2009
+#  Provided as-is, with no warranties
+#  License: http://creativecommons.org/licenses/by-nc-sa/3.0/us/ 
+
+import os
+import sys
+import re
+import datetime
+import time
+import commit_context
+# this import is only valid for Linux
+import commands
+# Import smtplib for the actual sending function
+import smtplib
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+
+def go(project_dir):
+    print 'Checking %s' % project_dir
+    # change to the project directory, necessary to find the .control file and
+    # to correctly refer to the project files by relative paths
+    os.chdir(project_dir)
+    # read the control file into a hashable set to compare git status entries
+    # more easily against the possible subset that should be controlled by the
+    # script
+    control_file = open('.control', 'r')
+    control_files = set()
+    feed = None
+    limit = None
+    author = None
+    email = None
+    notice_to = None
+    notice_from = None
+    try:
+        for line in control_file:
+            if line.startswith('#'):
+                continue
+            elif line.startswith('feed:'):
+                feed = line.split(':', 1)[1].strip()
+            elif line.startswith('limit:'):
+                limit = int(line.split(':')[1].strip())
+            elif line.startswith('author:'):
+                author = line.split(':')[1].strip()
+            elif line.startswith('notice_from:'):
+                notice_from = line.split(':')[1].strip()
+            elif line.startswith('notice_to:'):
+                notice_to = line.split(':')[1].strip()
+            else:
+                control_files.add(line.strip())
+    finally:
+        control_file.close()
+
+    if notice_from == None and notice_to != None:
+        notice_from = notice_to
+
+    if feed == None or limit == None or author == None or notice_to == None:
+        print 'Make sure that feed:, limit:, author:, and notice_to: are in the .control file'
+        sys.exit(1)
+
+    message_file = commit_context.build_message_file(feed, limit, author)
+    # get the git status for the project
+    git_status = commands.getoutput('git status')
+
+    # in particular find the existing entries that need a commit
+    pending_re = re.compile('#\s*(renamed|copied|modified):.*')
+
+    now = datetime.datetime.today()
+    quiet_period = datetime.timedelta(minutes=5)
+
+    # first look in the files git already knows about
+    for line in git_status.splitlines():
+        if pending_re.match(line):
+            pending_file = trim_git(line)
+            if not (pending_file in control_files):
+                continue
+            # remove files that will be committed
+            control_files.remove(pending_file)
+            last_mod = os.path.getmtime(pending_file)
+            pending_mod = datetime.datetime.fromtimestamp(last_mod)
+            pending_mod += quiet_period
+            if pending_mod < now:
+                commit_status = commands.getoutput('git commit -F ' + 
+                        message_file + ' ' + pending_file)
+                print commit_status
+            else:
+                print 'Change for file, %s, is too recent.' % pending_file
+
+    # find the control files that aren't in the project directory to notify that
+    # they need to be added
+    to_add = set()
+    for control_file in control_files:
+        if os.path.exists(control_file):
+            print '%s is unchanged since last check.' % control_file
+        else:
+            print '%s does not exist yet.' % control_file
+            to_add.add(control_file)
+    send_orphans(notice_to, notice_from, project_dir, to_add)
+
+def trim_git(status_line):
+    if status_line.find('->') >= 0:
+        tokens = status_line.split('->')
+        return tokens[1].strip()
+
+    tokens = status_line.split(':')
+    return tokens[1].strip()
+
+def send_orphans(notice_to, notice_from, project_dir, orphans):
+    body = 'The following files do not exist:\n'
+
+    for file in orphans:
+       body += '\t' + file + '\n'
+
+    # Create a text/plain message
+    msg = MIMEText(body, 'plain')
+
+    msg['Subject'] = 'Some files in %s do not exist' % project_dir
+    msg['From'] = notice_from
+    msg['To'] = notice_to
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP()
+    s.connect()
+    print 'Sending notice to %s.' % notice_to
+    s.sendmail(notice_from, [notice_to], msg.as_string())
+    s.close()
+
+# call go() when this module is executed as the main script
+if __name__ == "__main__":
+    project_dir = sys.argv[1]
+    go(project_dir)
