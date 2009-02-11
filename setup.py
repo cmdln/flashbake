6a6f10e11dc7d7afc3c0d8445ec2868a0d533f3c
Thomas Gideon <cmdln@thecommandline.net>
2009-02-11 12:26:46 -0500

    Got installation working.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from distutules.core import setup
+from distutils.core import setup
 
 setup(name='flashbake',
         version='0.14',
-        py_modules=['flashbake.commit'])
+        author="Thomas Gideon",
+        author_email="cmdln@thecommandline.net",
+        url="http://thecommandline.net",
+        license="http://creativecommons.org/licenses/by-nc-sa/3.0/us/",
+        py_modules=['flashbake.commit', 'flashbake.context'],
+        scripts=['bin/flashbake'])
