634794c0c679816d023626c939c077d314b82937
Thomas Gideon <cmdln@thecommandline.net>
2009-02-11 18:12:40 -0500

    Installation fixes.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 
 setup(name='flashbake',
-        version='0.14',
+        version='0.16',
         author="Thomas Gideon",
         author_email="cmdln@thecommandline.net",
         url="http://thecommandline.net",
         license="http://creativecommons.org/licenses/by-nc-sa/3.0/us/",
-        py_modules=['flashbake.commit', 'flashbake.context'],
+        py_modules=['flashbake.commit',
+            'flashbake.context',
+            'flashbake.plugins.feed',
+            'flashbake.plugins.timezone',
+            'flashbake.plugins.uptime',
+            'flashbake.plugins.weather'
+            ],
         scripts=['bin/flashbake'])
