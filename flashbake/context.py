79a2b55ecdf70abe07a9ff517c01f91cde4b9c46
Thomas Gideon <cmdln@thecommandline.net>
2009-02-11 16:16:34 -0500

    Re-factored feed into its own plugin.

--- flashbake/context.py
+++ flashbake/context.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python
 #
-#  commit-context.py
+#  context.py
 #  Build up some descriptive context for automatic commit to git
-#
-#  version 0.5 - fixed unsafe unicode handling
-#
-#  history
-#  version 0.4 - re-factored out ControlConfig
-#  version 0.3 - added checks for errors on network calls
-#  version 0.2 - added she-bang
-#  version 0.1 - functionally complete
-#
-#  Created by Thomas Gideon (cmdln@thecommandline.net) on 1/25/2009
-#  Provided as-is, with no warranties
-#  License: http://creativecommons.org/licenses/by-nc-sa/3.0/us/ 
 
 import sys
 import urllib, urllib2
@@ -112,51 +99,6 @@ def calcuptime():
 
     return string
 
-def filtertext(nodelist):
-    """ Filter out all but the text node children. """
-    text_value = ""
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            text_value = text_value + node.data
-    return text_value
-
-def fetchfeed(control_config):
-    """ Fetch up to the limit number of items from the specified feed with the specified
-        creator. """
-
-    # necessary machinery to fetch a web page
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-
-    try:
-        # open the raw RSS XML
-        rss_xml = opener.open(urllib2.Request(control_config.feed)).read()
-
-        # build a mini-dom so we can scrape out titles, descriptions
-        rss_dom = xml.dom.minidom.parseString(rss_xml)
-
-        titles = rss_dom.getElementsByTagName("title")
-
-        feed_title = filtertext(titles[0].childNodes)
-
-        items = rss_dom.getElementsByTagName("item")
-
-        by_creator = []
-        for child in items:
-           item_creator = child.getElementsByTagName("dc:creator")[0]
-           item_creator = filtertext(item_creator.childNodes).strip()
-           if item_creator != control_config.author:
-               continue
-           title = filtertext(child.getElementsByTagName("title")[0].childNodes)
-           title = title.encode('ascii', 'replace')
-           link = filtertext(child.getElementsByTagName("link")[0].childNodes)
-           by_creator.append({"title" : title, "link" : link})
-           if control_config.limit <= len(by_creator):
-               break
-
-        return (feed_title, by_creator)
-    except:
-        return (None, {})
-
 def buildmessagefile(control_config):
     """ Build a commit message that uses the provided ControlConfig object and
         return a reference to the resulting file. """
@@ -167,35 +109,33 @@ def buildmessagefile(control_config):
     # with thanks to Dave Smith
     uptime = calcuptime()
     
-    # last n items for m creator
-    (title,last_items) = fetchfeed(control_config)
-
     msg_filename = '/tmp/git_msg_%d' % random.randint(0,1000)
 
     # try to avoid clobbering another process running this script
     while os.path.exists(msg_filename):
         msg_filename = '/tmp/git_msg_%d' % random.randint(0,1000)
 
-    message_file = open(msg_filename, 'w')
+    plugins = list()
     __import__('flashbake.plugins.weather')
-    plugin = sys.modules['flashbake.plugins.weather']
+    plugins.append(sys.modules['flashbake.plugins.weather'])
+    __import__('flashbake.plugins.feed')
+    plugins.append(sys.modules['flashbake.plugins.feed'])
+
+    connected = False
+
+    message_file = open(msg_filename, 'w')
     try:
         message_file.write('Current time zone is %s\n' % zone)
-        weather_success = plugin.addcontext(message_file, control_config)
         if uptime == None:
             message_file.write('Couldn\'t determine up time.\n')
         else:
             message_file.write('System has been up %s\n' % uptime)
-        if len(last_items) > 0:
-            message_file.write('Last %(item_count)d entries from %(feed_title)s:\n'\
-                % {'item_count' : len(last_items), 'feed_title' : title})
-            for item in last_items:
-              # edit the '%s' if you want to add a label, like 'Title %s' to the output
-              message_file.write('%s\n' % item['title'])
-              message_file.write('%s\n' % item['link'])
-        else:
-            message_file.write('Couldn\'t fetch entries from feed, %s.\n' % control_config.feed)
-        if not weather_success and len(last_items) == 0:
+        for plugin in plugins:
+            plugin_success = plugin.addcontext(message_file, control_config)
+            # TODO track this only for connected plugins
+            if plugin_success:
+               connected = True
+        if not connected:
             message_file.write('System is most likely offline.')
     finally:
         message_file.close()
