3edb69c54ac606f4a2c2a62acae985ff3534bad8
Thomas Gideon <lyon@mugen.local>
2009-02-12 09:40:43 -0500

    A few more fixes, inclduing correcting typos.

--- flashbake/commit.py
+++ flashbake/commit.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python
 #
-#  commit-wrapper.py
+#  commit.py
 #  Parses a project's control file and wraps git operations, calling the context
 #  script to build automatic commit messages as needed.
-#
-#  version 0.17 - installation fixes
-#
-#  history:
-#  version 0.16 - installation fixes
-#  version 0.15 - groundwork for 3rd party plugins
-#  version 0.14 - packaging and setup
-#  version 0.13 - code clean up
-#  version 0.12 - added error checks to network calls
-#  version 0.11 - use a regex to fix trailing tweedle causing false reports
-#  version 0.10 - added link check, added logic to add files
-#  version 0.9 - added a trap for a fatal error from git
-#  version 0.8 - more logging changes
-#  version 0.7 - more logging changes
-#  version 0.6 - improved logging, more quoting of arguments to shell
-#  version 0.5 - consolidate commits
-#  version 0.4 - Added quotes to git call for filenames with spaces
-#  version 0.3 - SMTP port, she-bang
-#  version 0.2 - email notification
-#  version 0.1 - git functionality complete
 #
 #  Created by Thomas Gideon (cmdln@thecommandline.net) on 1/25/2009
 #  Provided as-is, with no warranties
