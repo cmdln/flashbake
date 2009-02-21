e7e639e37bea9a3c5d53219507014db5d105bcfb
Thomas Gideon <cmdln@thecommandline.net>
2009-02-21 15:28:41 -0500

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='flashbake',
-        version='0.19',
+        version='0.20',
         author="Thomas Gideon",
         author_email="cmdln@thecommandline.net",
         url="http://thecommandline.net",
