4fd279739540ff1c1b28afef8b4b13fcb3ad88f2
Thomas Gideon <cmdln@thecommandline.net>
2009-04-12 13:44:17 -0400

    Got IP based location working, need to add caching to smooth out load on server.

--- flashbake/plugins/location.py
+++ flashbake/plugins/location.py
@@ -2,14 +2,77 @@
 #  Net location plugin.
 
 import logging
+import urllib
 import urllib2
 import re
 from urllib2 import HTTPError, URLError
 from flashbake.plugins import AbstractMessagePlugin
+import xml
 
 class Location(AbstractMessagePlugin):
     def addcontext(self, message_file, config):
-        # unpublished API that iGoogle uses for its weather widget
+        ip_addr = self.__get_ip()
+        if ip_addr == None:
+            message_file.write('Failed to get public IP for geo location.\n')
+            return False
+        location = self.__locate_ip(ip_addr)
+        if len(location) == 0:
+            message_file.write('Failed to parse location data for IP address.\n')
+            return False
+
+        logging.debug(location)
+        location_str = '%(City)s, %(RegionName)s' % location
+        if 'weather_city' in config.__dict__:
+            config.weather_city = location_str
+        message_file.write('Current location is %s based on IP %s.\n' % (location_str, ip_addr))
+
+    def __locate_ip(self, ip_addr):
+        # TODO cache IP, check for same in the cache
+        base_url = 'http://iplocationtools.com/ip_query.php?'
+        for_ip = base_url + urllib.urlencode({'ip': ip_addr})
+
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+
+        try:
+            logging.debug('Requesting page for %s.' % for_ip)
+
+            # open the location API page
+            location_xml = opener.open(urllib2.Request(for_ip)).read()
+
+            # the weather API returns some nice, parsable XML
+            location_dom = xml.dom.minidom.parseString(location_xml)
+
+            # just interested in the conditions at the moment
+            response = location_dom.getElementsByTagName("Response")
+
+            if response == None or len(response) == 0:
+                return dict()
+
+            location = dict()
+            for child in response[0].childNodes:
+                if child.localName == None:
+                    continue
+                key = child.localName
+                key = key.encode('ASCII', 'replace')
+                location[key] = self.__get_text(child.childNodes)
+
+            return location
+        except HTTPError, e:
+            logging.error('Failed with HTTP status code %d' % e.code)
+            return {}
+        except URLError, e:
+            logging.error('Failed with reason %s.' % e.reason)
+            return {}
+
+    def __get_text(self, node_list):
+        text_value = ''
+        for node in node_list:
+            if node.nodeType != node.TEXT_NODE:
+                continue;
+            text_value += node.data
+        return text_value
+
+    def __get_ip(self):
         no_reply = 'http://www.noreply.org'
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 
@@ -28,13 +91,10 @@ class Location(AbstractMessagePlugin):
             if m == None:
                 message_file.write('Failed to parse Hello with public IP address.')
             ip_addr = m.group(0)
-            logging.debug(ip_addr)
-            return True
+            return ip_addr
         except HTTPError, e:
             logging.error('Failed with HTTP status code %d' % e.code)
-            message_file.write('Failed to get public IP for geo location.\n')
-            return False
+            return None
         except URLError, e:
             logging.error('Failed with reason %s.' % e.reason)
-            message_file.write('Failed to get public IP for geo location.\n')
-            return False
+            return None
