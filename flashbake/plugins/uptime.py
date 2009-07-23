783ac8d29d09d0ab068f41d53601d446ac4ee945
Thomas Gideon <cmdln@thecommandline.net>
2009-07-23 15:19:18 -0400

    re-factored up time plugins.

--- flashbake/plugins/uptime.py
+++ flashbake/plugins/uptime.py
@@ -17,15 +17,19 @@
 
 '''  uptime.py - Stock plugin to calculate the system's uptime and add to the commit message.'''
 
-import string
-import os.path
-import logging
-from subprocess import Popen, PIPE
 from flashbake.plugins import AbstractMessagePlugin
+from subprocess import Popen, PIPE
+import logging
+import os.path
+import string
+
+
+
 
 class UpTime(AbstractMessagePlugin):
     def init(self, config):
         """ Nothing needed. """
+
 
     def addcontext(self, message_file, config):
         """ Add the system's up time to the commit context. """
@@ -39,6 +43,7 @@ class UpTime(AbstractMessagePlugin):
 
         return True
 
+
     def __calcuptime(self):
         """ copied with blanket permission from
             http://thesmithfam.org/blog/2005/11/19/python-uptime-script/ """
@@ -46,7 +51,7 @@ class UpTime(AbstractMessagePlugin):
         if not os.path.exists('/proc/uptime'):
             return self.__run_uptime()
 
-        f = open( "/proc/uptime" )
+        f = open("/proc/uptime")
         try:
              contents = f.read().split()
         except:
@@ -57,33 +62,36 @@ class UpTime(AbstractMessagePlugin):
         total_seconds = float(contents[0])
 
         # Helper vars:
-        MINUTE  = 60
-        HOUR    = MINUTE * 60
-        DAY     = HOUR * 24
+        MINUTE = 60
+        HOUR = MINUTE * 60
+        DAY = HOUR * 24
 
         # Get the days, hours, etc:
-        days    = int( total_seconds / DAY )
-        hours   = int( ( total_seconds % DAY ) / HOUR )
-        minutes = int( ( total_seconds % HOUR ) / MINUTE )
-        seconds = int( total_seconds % MINUTE )
+        days = int(total_seconds / DAY)
+        hours = int((total_seconds % DAY) / HOUR)
+        minutes = int((total_seconds % HOUR) / MINUTE)
+        seconds = int(total_seconds % MINUTE)
 
         # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
         string = ""
-        if days> 0:
-            string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
-        if len(string)> 0 or hours> 0:
-            string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
-        if len(string)> 0 or minutes> 0:
-            string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
-        string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
+        if days > 0:
+            string += str(days) + " " + (days == 1 and "day" or "days") + ", "
+        if len(string) > 0 or hours > 0:
+            string += str(hours) + " " + (hours == 1 and "hour" or "hours") + ", "
+        if len(string) > 0 or minutes > 0:
+            string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes") + ", "
+        string += str(seconds) + " " + (seconds == 1 and "second" or "seconds")
 
         return string
-    
-    
+
+
     def __run_uptime(self):
         """ For OSes that don't provide procfs, then try to use the updtime command.
         
             Thanks to Tony Giunta for this contribution. """
+        if not flashbake.executable_available('uptime'):
+            return None
+
         # Try to capture output of 'uptime' command, 
         # if not found, catch OSError, log and return None
         try:
@@ -91,10 +99,10 @@ class UpTime(AbstractMessagePlugin):
         except OSError:
             logging.warn("Can't find 'uptime' command in $PATH")
             return None
-    
+
         # Parse uptime output string
         # if len == 10 or 11, uptime is less than a day
-        if len(output) in [10,11]:
+        if len(output) in [10, 11]:
             days = "00"
             hours_and_minutes = output[2].strip(",")
         elif len(output) == 12:
@@ -102,7 +110,7 @@ class UpTime(AbstractMessagePlugin):
             hours_and_minutes = output[4].strip(",")
         else:
             return None
-        
+
         # If time is exactly x hours/mins, no ":" in "hours_and_minutes" 
         # and the interpreter will throw a ValueError
         try:
@@ -116,11 +124,11 @@ class UpTime(AbstractMessagePlugin):
                 minutes = hours_and_minutes
             else:
                 return None
-        
+
         # Build up output string, might require Python 2.5+
         uptime = (days + (" day, " if days == "1" else " days, ") +
                 hours + (" hour, " if hours == "1" else " hours, ") +
                 minutes + (" minute" if minutes == "1" else " minutes"))
-    
+
         return uptime
 
