d9b3cbdea5322de596f435dcb30fba8c0156a5fd
Thomas Gideon <cmdln@thecommandline.net>
2009-11-06 15:02:03 -0500

    Merged in iTunes script.

--- flashbake/plugins/music.py
+++ flashbake/plugins/music.py
@@ -14,14 +14,19 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with flashbake.  If not, see <http://www.gnu.org/licenses/>.
+#
+# the iTunes class is based on the itunes.py by Andrew Wheiss, originally
+# licensed under an MIT License
 
 '''  music.py - Plugin for gathering last played tracks from music player. '''
 
-import sqlite3
-import os.path
+from flashbake.plugins import AbstractMessagePlugin, AbstractMessagePlugin
 import logging
+import os
+import os.path
+import sqlite3
+import string
 import time
-from flashbake.plugins import AbstractMessagePlugin
 
 
 
@@ -66,3 +71,37 @@ limit %d"""
             conn.close()
 
         return True
+
+
+class iTunes(AbstractMessagePlugin):
+    ''' Based on Andrew Wheiss' plugin which is MIT licensed which should be compatible. '''
+    def __init__(self):
+        self.define_property('osascript')
+        
+    def init(self, config):
+        if self.osascript is None:
+            self.osascript = flashbake.findexecutable('osascript')
+            
+    def addcontext(self, message_file, config):
+        """ Get the track info and write it to the commit message """
+        info = self.trackinfo()
+
+        if info is None:
+            message_file.write('Couldn\'t get current track.\n')
+        else:
+            message_file.write('%s' % info)
+
+        return True
+
+    def trackinfo():
+        ''' Call the AppleScript file. '''
+        if self.osascript is None:
+            return None
+        directory = os.path.dirname(__file__)
+        script_path = os.path.join(directory, 'current_track.scpt')
+        
+        args = [self.osascript, script_path]
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                             close_fds = True)
+        
+        return proc.communicate()[0]
