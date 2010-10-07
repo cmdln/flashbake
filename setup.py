b1703207e186b0fddb57735a4b7b9c227aeff1a0
Thomas Gideon <cmdln@thecommandline.net>
2010-10-07 11:59:33 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flashbake',
-        version='0.26.2',
+        version='0.26.3',
         author="Thomas Gideon",
         author_email="cmdln@thecommandline.net",
         url="http://thecommandline.net",
