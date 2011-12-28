49f1d7ea8dccdf1d7f5e892fc8f6afcded066113
cmdln <cmdln@thecommandline.net>
2011-12-28 16:22:47 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flashbake',
-        version='0.26.3',
+        version='0.27',
         author="Thomas Gideon",
         author_email="cmdln@thecommandline.net",
         maintainer="Thomas Gideon",
