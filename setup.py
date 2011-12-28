951f2ea58b09661c22140bfc7f1cbeafe13a1010
cmdln <cmdln@thecommandline.net>
2011-12-28 16:11:59 -0500

    Add metadata for pypi submission.

--- setup.py
+++ setup.py
@@ -9,7 +9,27 @@ setup(name='flashbake',
         version='0.26.3',
         author="Thomas Gideon",
         author_email="cmdln@thecommandline.net",
+        maintainer="Thomas Gideon",
+        maintainer_email="cmdln@thecommandline.net",
+        description="Automation to feed life log into version control message stream.",
+        long_description=""" Flashbake was designed to help technically savvy
+writers use version control by compiling information from the variety of
+sources that make up the user's life log and automating the inclusion of
+that information in a commit stream, as part of the messages in the
+history.""",
+        platforms=[ "noarch" ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Artistic Software'
+        ],
         url="http://thecommandline.net",
+        download_url="https://github.com/commandline/flashbake/downloads",
         license="GPLv3",
         packages=find_packages(exclude=['test.*']),
         install_requires='''
