4196f9765c13c5c499b24238667d0e7d55800871
Ian Paul <ian@ianpaul.net>
2017-03-19 21:53:33 +0200

    screwing up with the new twitter file

--- twitter.py
+++ twitter.py
@@ -22,11 +22,20 @@
     As of 2017, users must use this script to capture their tweets. '''
 
 from flashbake.plugins import AbstractMessagePlugin
-from urllib2 import HTTPError, URLError
-from xml.etree.ElementTree import ElementTree
 import logging
 import urllib
-import tweepy
+from twython import Twython
+
+class Tweeter(AbstractMessagePlugin):
+      def __init__(self,plugin_spec):
+          AbstractMessagePlugin.__init__(self, plugin_spec, True)
+          self.define_property('cons_key', required=False)
+          self.define_property('cons_sec', required=False)
+
+      def addcontext(self, message_file, config)
+          if self.cons_key == None and self.cons_sec == None:
+              message_file.write('Sorry, you need a consumer key and secret to commit your tweets')
+              return False
 
 
 
