1db0bcf51b2335fc015d88e59fe40dec15be3e9f
Ian Paul <ian@ianpaul.net>
2021-07-31 17:45:39 +0300

    More print strings (#52)
    * changed line 132 to have str.format()
    
    * Adjusted lines 41-47
    
    * Changed line 52 to str.format()
    
    * Revert "changed line 132 to have str.format()"
    
    This reverts commit 05c17f9791ff8bb2fc8b03ebf7c35aa97190c07a.
    
    * Revert "Adjusted lines 41-47"
    
    This reverts commit 5fd636784e6f2efbbe0acd3e795669258a9fbdae.
    
    * added more str.format() to feed.py
    
    * changed dictionary to list on lines 43 and 46
    
    * Added new version number (0.30.0) and minor upates to project.zip

--- src/flashbake/plugins/feed.py
+++ src/flashbake/plugins/feed.py
@@ -39,17 +39,17 @@ class Feed(AbstractMessagePlugin):
 
         if len(last_items) > 0:
             if self.author == None:
-                message_file.write('Last %(item_count)d entries from %(feed_title)s:\n'\
-                    % {'item_count' : len(last_items), 'feed_title' : title})
+                message_file.write('Last {} entries from {}:\n'\
+                    .format(*[len(last_items), title]))
             else:
-                message_file.write('Last %(item_count)d entries from %(feed_title)s by %(author)s:\n'\
-                    % {'item_count' : len(last_items), 'feed_title' : title, 'author' or 'dc:creator' : self.author})
+                message_file.write('Last {} entries from {} by {}:\n'\
+                    .format(*[len(last_items), title, self.author]))
             for item in last_items:
-                # edit the '%s' if you want to add a label, like 'Title %s' to the output
+                
                 message_file.write(f'{item["title"]}\n')
                 message_file.write(f'{item["link"]}\n')
         else:
-            message_file.write('Couldn\'t fetch entries from feed, %s.\n' % self.url)
+            message_file.write('Couldn\'t fetch entries from feed, {}\n'.format(self.url))
 
         return len(last_items) > 0
 
