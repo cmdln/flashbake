d45598874726e9fad48d7962120242a5ecd36f51
Ian Paul <ian@ianpaul.net>
2022-05-23 18:41:22 +0000

    swapped out a for loop for a list comprehension.

--- src/flashbake/plugins/lastlog.py
+++ src/flashbake/plugins/lastlog.py
@@ -11,14 +11,17 @@ class LastLog(AbstractMessagePlugin):
         #res1 = [item for items in llog for item in items if target in item] 
         #for i in range(len(llog)):
         #matching = [s for s in llog if target in s]
-        x = []
-        for s in llog:
-            for i in s:
-                if target in i:
-                    x.append(s)
+         
+        #x = []
+        #for s in llog:
+        x = [s for s in llog for i in s if target in i] 
+            #for i in s:
+            #    if target in i:
+            #        x.append(s)
         print(x)
         for i in range(len(x)):
             message_file.write("{0} {1} {2} {3} {4} {5}\n".format(x[i][0], x[i][3], x[i][4], x[i][5], x[i][8], x[i][6]))
+    
     def checklog(self):
         last = subprocess.run(["lastlog"], capture_output=True,
                 text=True).stdout.strip("\n")
