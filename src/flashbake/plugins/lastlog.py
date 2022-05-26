759f036c9efe65dad982dc35587b889882b4f70e
Ian Paul <ian@ianpaul.net>
2022-05-26 15:07:00 +0000

    Added a variable to lastlog so people can set how far back they want the lastlog reports to go.

--- src/flashbake/plugins/lastlog.py
+++ src/flashbake/plugins/lastlog.py
@@ -5,6 +5,7 @@ import datetime
 class LastLog(AbstractMessagePlugin):
     def __init__(self, plugin_spec):
         AbstractMessagePlugin.__init__(self, plugin_spec)
+        self.define_property('interval', int, required=False)
 
     def addcontext(self, message_file, config):
         llog = self.checklog()
@@ -12,22 +13,16 @@ class LastLog(AbstractMessagePlugin):
         ''' loop through llog and store a list of lists that contain a
         colon to the variable x.'''
         x = [s for s in llog for i in s if target in i] 
-        
-        #for i in range(len(x)):
-        #    message_file.write("{0} {1} {2} {3} {4} {5}\n".format(x[i][0], x[i][3], x[i][4], x[i][5], x[i][8], x[i][6]))
-        #for i in x:
-        #    print(i[8]) 
+        if self.interval == None:
+            self.interval = 24
         for i in x:
             date_placement = [i[3], i[4], i[5], i[6], i[8]]
             y = ' '.join(date_placement)
-            #print(date_placement)
-            past24 = datetime.datetime.now() - datetime.timedelta(hours=24)
-            #if datetime.datetime(*date_placement[2:4]) <= past24:
-            #    print(i)
+            past24 = datetime.datetime.now() - datetime.timedelta(hours=self.interval)
             if datetime.datetime.strptime(y, "%a %b %d %H:%M:%S %Y") >= past24:
-            #    print(i)
                 message_file.write(f'Most recent lastlog entries: \n{i[0]} {i[3]} {i[4]} {i[5]} {i[6]} {i[8]}')
     def checklog(self):
+        ''' Get the lastlog and turn each line into a list of lists. '''
         last = subprocess.run(["lastlog"], capture_output=True,
                 text=True).stdout.strip("\n")
         llog = []
